d3fffd18e47cfcd2c9809bdf8ff8d6cff6074936
Dan <up765064@myport.ac.uk>
2020-12-30 10:59:50 +0000

    added_cors_changed_scripts

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-// "dev": "nodemon -r dotenv/config ./bin/www",
 
 const { getAllVideos, addVideo } = require("../model/index");
 

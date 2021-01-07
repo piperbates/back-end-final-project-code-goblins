b2dcd0b06b3f9945e8cdc29d3568efcdd659b5c3
moms-spaghetti <williamedwards36@aol.com>
2021-01-07 05:29:27 +0000

    added resetVideoTable script

---
+++ db/sampleData/videoData.js
@@ -0,0 +1,326 @@
+const sampleData = [
+  {
+    title: "Wondr Guest Presentation",
+    lecturer: "Simon and David",
+    video_url: "https://vimeo.com/495726092",
+    thumbnail_url: "https://i.vimeocdn.com/video/1025552077_295x166.jpg?r=pad",
+    tags: ["wondr", "guest lecture", "agile", "teams", "startups"],
+    timestamps: [
+      {
+        timeDesc: "Start",
+        timeString: "00:00:00",
+        timeSecondsValue: 0,
+      },
+      {
+        timeDesc: "Our Journey",
+        timeString: "00:13:39",
+        timeSecondsValue: 300,
+      },
+      {
+        timeDesc: "Our Company",
+        timeString: "00:34:30",
+        timeSecondsValue: 2070,
+      },
+      {
+        timeDesc: "Final Words",
+        timeString: "00:49:37",
+        timeSecondsValue: 2977,
+      },
+    ],
+    lecture_date: "2020-12-17",
+    bootcamp_week: 13,
+    description: "Simon and David from Wondr talk about their startup journey",
+    github_links: [
+      {
+        link: "https://github.com/SchoolOfCode/",
+        desc: "workshop 1",
+        type: "github",
+      },
+      {
+        link: "https://github.com/SchoolOfCode/",
+        desc: "workshop 2",
+        type: "github",
+      },
+    ],
+    slides: [
+      {
+        link: "http://google.com",
+        desc: "am slide",
+        type: "presentation slide",
+      },
+      {
+        link: "http://google.com",
+        desc: "pm slide",
+        type: "presentation slide",
+      },
+    ],
+    other_links: [
+      {
+        link: "https://site.mywondr.co/",
+        desc: "mywondr.com",
+        type: "additional reading",
+      },
+    ],
+  },
+  {
+    title: "AWS Serverless",
+    lecturer: "Liz Kauffmann",
+    video_url: "https://vimeo.com/495725984",
+    thumbnail_url: "https://i.vimeocdn.com/video/1025552912_295x166.jpg?r=pad",
+    tags: ["serverless", "aws", "cloud"],
+    timestamps: [
+      {
+        timeDesc: "Introduing AWS",
+        timeString: "00:00:00",
+        timeSecondsValue: 0,
+      },
+      {
+        timeDesc: "More on sererless functions",
+        timeString: "00:26:36",
+        timeSecondsValue: 1596,
+      },
+      {
+        timeDesc: "Lambda functions",
+        timeString: "01:17:35",
+        timeSecondsValue: 4655,
+      },
+      {
+        timeDesc: "Putting it all together",
+        timeString: "01:46:15",
+        timeSecondsValue: 6375,
+      },
+      {
+        timeDesc: "What next",
+        timeString: "02:17:22",
+        timeSecondsValue: 8242,
+      },
+    ],
+    lecture_date: "2020-12-11",
+    bootcamp_week: 12,
+    description: "A workshop on getting a serverless API up and running",
+    github_links: [
+      {
+        link: "https://github.com/SchoolOfCode/",
+        desc: "workshop 1",
+        type: "github",
+      },
+      {
+        link: "https://github.com/SchoolOfCode/",
+        desc: "workshop 2",
+        type: "github",
+      },
+    ],
+    slides: [
+      {
+        link: "http://google.com",
+        desc: "am slide",
+        type: "presentation slide",
+      },
+      {
+        link: "http://google.com",
+        desc: "pm slide",
+        type: "presentation slide",
+      },
+    ],
+    other_links: [
+      {
+        link: "https://aws.amazon.com/",
+        desc: "aws",
+        type: "additional reading",
+      },
+    ],
+  },
+  {
+    title: "Docker Fundamentals",
+    lecturer: "Tao Sharma",
+    video_url: "https://vimeo.com/489797788",
+    thumbnail_url: "https://i.vimeocdn.com/video/1013185470_295x166.jpg?r=pad",
+    tags: ["docker", "servers", "cloud"],
+    timestamps: [
+      {
+        timeDesc: "Introducing Docker",
+        timeString: "00:00:00",
+        timeSecondsValue: 0,
+      },
+      {
+        timeDesc: "Containers vs Serverless",
+        timeString: "00:06:30",
+        timeSecondsValue: 390,
+      },
+      {
+        timeDesc: "Leveraging the power of containers",
+        timeString: "00:18:54",
+        timeSecondsValue: 1134,
+      },
+    ],
+    lecture_date: "2020-12-10",
+    bootcamp_week: 12,
+    description: "A workshop on Docker and getting it up and running",
+    github_links: [
+      {
+        link: "https://github.com/SchoolOfCode/",
+        desc: "workshop 1",
+        type: "github",
+      },
+      {
+        link: "https://github.com/SchoolOfCode/",
+        desc: "workshop 2",
+        type: "github",
+      },
+    ],
+    slides: [
+      {
+        link: "http://google.com",
+        desc: "am slide",
+        type: "presentation slide",
+      },
+      {
+        link: "http://google.com",
+        desc: "pm slide",
+        type: "presentation slide",
+      },
+    ],
+    other_links: [
+      {
+        link: "https://docker.com/",
+        desc: "docker",
+        type: "additional reading",
+      },
+    ],
+  },
+  {
+    title: "useReducer Review (Recap Sesssion)",
+    lecturer: "Liz Kaufmann",
+    video_url: "https://vimeo.com/489792489",
+    thumbnail_url: "https://i.vimeocdn.com/video/1013197144_295x166.jpg?r=pad",
+    tags: ["react", "usereducer", "functions", "javascript"],
+    timestamps: [
+      {
+        timeDesc: "Start",
+        timeString: "00:00:00",
+        timeSecondsValue: 0,
+      },
+      {
+        timeDesc: "Deep dive on use reducer",
+        timeString: "00:05:30",
+        timeSecondsValue: 330,
+      },
+      {
+        timeDesc: "Further React hooks",
+        timeString: "00:21:20",
+        timeSecondsValue: 1280,
+      },
+      {
+        timeDesc: "Advanced techniques",
+        timeString: "00:42:49",
+        timeSecondsValue: 2569,
+      },
+    ],
+    lecture_date: "2020-11-10",
+    bootcamp_week: 10,
+    description: "A recap of useReducer",
+    github_links: [
+      {
+        link: "https://github.com/SchoolOfCode/",
+        desc: "workshop 1",
+        type: "github",
+      },
+      {
+        link: "https://github.com/SchoolOfCode/",
+        desc: "workshop 2",
+        type: "github",
+      },
+    ],
+    slides: [
+      {
+        link: "http://google.com",
+        desc: "am slide",
+        type: "presentation slide",
+      },
+      {
+        link: "http://google.com",
+        desc: "pm slide",
+        type: "presentation slide",
+      },
+    ],
+    other_links: [
+      {
+        link: "https://reactjs.org/docs/getting-started.html",
+        desc: "react getting started",
+        type: "additional reading",
+      },
+    ],
+  },
+  {
+    title: "JWT Authentication",
+    lecturer: "Ben Lee",
+    video_url: "https://vimeo.com/489792198",
+    thumbnail_url: "https://i.vimeocdn.com/video/1013175389_295x166.jpg?r=pad",
+    tags: ["node", "authentication", "jwt", "tokens"],
+    timestamps: [
+      {
+        timeDesc: "What are JWTs?",
+        timeString: "00:00:00",
+        timeSecondsValue: 0,
+      },
+      {
+        timeDesc: "The inner workings of tokens",
+        timeString: "00:07:10",
+        timeSecondsValue: 430,
+      },
+      {
+        timeDesc: "Security essentials",
+        timeString: "00:13:50",
+        timeSecondsValue: 830,
+      },
+      {
+        timeDesc: "Workshop review",
+        timeString: "00:28:47",
+        timeSecondsValue: 1727,
+      },
+    ],
+    lecture_date: "2020-11-06",
+    bootcamp_week: 10,
+    description: "Implementing and using JWT",
+    github_links: [
+      {
+        link: "https://github.com/SchoolOfCode/",
+        desc: "workshop 1",
+        type: "github",
+      },
+      {
+        link: "https://github.com/SchoolOfCode/",
+        desc: "workshop 2",
+        type: "github",
+      },
+    ],
+    slides: [
+      {
+        link: "http://google.com",
+        desc: "am slide",
+        type: "presentation slide",
+      },
+      {
+        link: "http://google.com",
+        desc: "pm slide",
+        type: "presentation slide",
+      },
+    ],
+    other_links: [
+      {
+        link: "https://www.npmjs.com/package/jsonwebtoken",
+        desc: "npm jwt site",
+        type: "additional reading",
+      },
+    ],
+    other_links: [
+      {
+        link: "http://www.passportjs.org/packages/passport-jwt/",
+        desc: "passport js",
+        type: "additional reading",
+      },
+    ],
+  },
+];
+
+module.exports = sampleData;

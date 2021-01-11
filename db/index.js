f572fb056c93040ca4f5288ceeb74097652dd477
Piper <bates.sarah42@gmail.com>
2021-01-11 17:23:57 +0000

    deploying

--- db/index.js
+++ db/index.js
@@ -3,11 +3,12 @@ dotenv.config();
 const { Pool } = require("pg");
 //remember dotenv in full build
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  // user: process.env.PGUSER,
+  // host: process.env.PGHOST,
+  // database: process.env.PGDATABASE,
+  // password: process.env.PGPASSWORD,
+  // port: process.env.PGPORT,
   ssl: { rejectUnauthorized: false },
 });
 

dbbfec5c9a9014ae719bcf8d5a7eb753845b66a3
moms-spaghetti <williamedwards36@aol.com>
2021-01-15 14:21:58 +0000

    config file added for model db, minor changes to layout in routes

--- model/index.js
+++ model/index.js
@@ -5,28 +5,25 @@ let client = new Vimeo(
   process.env.VIMEO_SECRET,
   process.env.VIMEO_TOKEN
 );
+const { config } = require("../config");
 
 //Get all videos
 async function getAllVideos() {
-  const result = await query(`SELECT * FROM videos ;`);
+  const result = await query(`SELECT * FROM ${config.DATABASE_VIDEOS} ;`);
   return result.rows;
 }
 
 //Get filtered videos from the search query URL parameters
 async function getFilteredVideos(urlQueries) {
-  // Destructure url search parameters
   let { search, lecturer, week, tag } = urlQueries;
-  // Pre-format search string if it exists
   search ? (search = `%${search}%`) : (search = `%%`);
-  // Pre-format lecturer string if it exists
   lecturer ? (lecturer = `%${lecturer}%`) : (lecturer = "%%");
-  // Pre-format tags if they exist
   tag
     ? (tag = "['" + (Array.isArray(tag) ? tag.join("','") : tag) + "']")
     : (tag = "[]");
 
   const result = await query(
-    `SELECT * FROM videos
+    `SELECT * FROM ${config.DATABASE_VIDEOS}
       WHERE (description ILIKE $1 OR title ILIKE $1)
       AND (lecturer ILIKE $2)
       AND (bootcamp_week = COALESCE($3, bootcamp_week))
@@ -38,7 +35,10 @@ async function getFilteredVideos(urlQueries) {
 
 // Get a specific video by ID
 async function getVideoById(id) {
-  const result = await query("SELECT * FROM videos WHERE id = $1;", [id]);
+  const result = await query(
+    `SELECT * FROM ${config.DATABASE_VIDEOS} WHERE id = $1;`,
+    [id]
+  );
   return result.rows;
 }
 
@@ -59,7 +59,7 @@ async function addVideo(
   other_links
 ) {
   const result = await query(
-    `INSERT INTO videos (title,
+    `INSERT INTO ${config.DATABASE_VIDEOS} (title,
       lecturer,
       video_url,
       thumbnail_url,
@@ -93,69 +93,58 @@ async function addVideo(
 
 //Delete video
 async function deleteVideo(id) {
-  const result = await query(`DELETE FROM videos WHERE id = ($1);`, [id]);
+  const result = await query(
+    `DELETE FROM ${config.DATABASE_VIDEOS} WHERE id = $1;`,
+    [id]
+  );
   return result.rows;
 }
 
 //Update video
-async function updateVideo(
-  title,
-  lecturer,
-  video_url,
-  thumbnail_url,
-  tags,
-  timestamps,
-  lecture_date,
-  bootcamp_week,
-  cohort,
-  description,
-  github_links,
-  slides,
-  other_links,
-  id
-) {
+async function updateVideo(data) {
+  console.log(data.id);
   const result = await query(
-    `UPDATE videos SET
-  title = COALESCE($1, title),
-  lecturer = COALESCE($2, lecturer),
-  video_url = COALESCE($3, video_url),
-  thumbnail_url = COALESCE($4, thumbnail_url),
-  tags = COALESCE($5, tags),
-  timestamps = COALESCE($6, timestamps),
-  lecture_date = COALESCE($7, lecture_date),
-  bootcamp_week = COALESCE($8, bootcamp_week),
-  cohort = COALESCE($9, bootcamp_week),
-  description = COALESCE($10, description),
-  github_links = COALESCE($11, github_links),
-  slides = COALESCE($12, slides),
-  other_links = COALESCE($13, other_links) 
-  WHERE id = ($14) 
-  RETURNING id;`,
+    `UPDATE ${config.DATABASE_VIDEOS}
+      SET title = $1,
+          lecturer = $2,
+          video_url = $3,
+          thumbnail_url = $4,
+          tags = $5,
+          timestamps = $6,
+          lecture_date = $7,
+          bootcamp_week = $8,
+          cohort = $9,
+          description = $10,
+          github_links = $11,
+          slides = $12,
+          other_links = $13 
+      WHERE id = $14
+    `,
     [
-      title,
-      lecturer,
-      video_url,
-      thumbnail_url,
-      tags,
-      timestamps,
-      lecture_date,
-      bootcamp_week,
-      cohort,
-      description,
-      github_links,
-      slides,
-      other_links,
-      id,
+      data.title,
+      data.lecturer,
+      data.video_url,
+      data.thumbnail_url,
+      data.tags,
+      data.timestamps,
+      data.lecture_date,
+      data.bootcamp_week,
+      data.cohort,
+      data.description,
+      data.github_links,
+      data.slides,
+      data.other_links,
+      data.id,
     ]
   );
-  return result.rows[0].id;
+  return result.rows[0];
 }
 
 /* FEEDBACK BUTTON */
 //get all feedback
 async function getAllFeedback() {
   const res = await query(`
-  SELECT * FROM feedbacktable
+  SELECT * FROM ${config.DATABASE_FEEDBACK}
     `);
   return res.rows;
 }
@@ -165,7 +154,7 @@ async function addNewFeedback(value) {
   console.log({ value });
   const res = await query(
     `
-      INSERT INTO feedbackTable (
+      INSERT INTO ${config.DATABASE_FEEDBACK} (
         videoid,
         feedback
         )
@@ -201,31 +190,31 @@ const getVimeoVideoData = async (query) => {
 /*** tag functions ***/
 
 const getAllTagData = async () => {
-  const sql = `SELECT * FROM tags ORDER BY tag ASC`;
+  const sql = `SELECT * FROM ${config.DATABASE_TAGS} ORDER BY tag ASC`;
   const response = await query(sql);
   return response.rows;
 };
 
 const deleteTag = async (tag) => {
-  const sql = `DELETE FROM tags WHERE key = $1`;
+  const sql = `DELETE FROM ${config.DATABASE_TAGS} WHERE key = $1`;
   const response = await query(sql, [tag]);
   return response.rows;
 };
 
 const addTag = async (tag) => {
-  const sql = `INSERT INTO tags (tag) VALUES (LOWER($1))`;
+  const sql = `INSERT INTO ${config.DATABASE_TAGS} (tag) VALUES (LOWER($1))`;
   const response = await query(sql, [tag]);
   return response.rows;
 };
 
 const updateTag = async (tag) => {
-  const sql = `UPDATE tags SET tag = LOWER($1) WHERE key = $2`;
+  const sql = `UPDATE tags SET ${config.DATABASE_TAGS} = LOWER($1) WHERE key = $2`;
   const response = await query(sql, [tag.updateValue, tag.updateRecord]);
   return response.rows;
 };
 
 const getNextTagVal = async () => {
-  const sql = `SELECT last_value FROM tags_key_seq`;
+  const sql = `SELECT last_value FROM ${config.DATABASE_TAGKEYSEQ}`;
   const response = await query(sql);
   return response.rows;
 };
@@ -233,25 +222,25 @@ const getNextTagVal = async () => {
 /*** lecture functions ***/
 
 const getAllLecturerData = async () => {
-  const sql = `SELECT * FROM lecturers ORDER BY lecturer ASC`;
+  const sql = `SELECT * FROM ${config.DATABASE_LECTURERS} ORDER BY lecturer ASC`;
   const response = await query(sql);
   return response.rows;
 };
 
 const deleteLecturer = async (lecturer) => {
-  const sql = `DELETE FROM lecturers WHERE key = $1`;
+  const sql = `DELETE FROM ${config.DATABASE_LECTURERS} WHERE key = $1`;
   const response = await query(sql, [lecturer]);
   return response.rows;
 };
 
 const addLecturer = async (lecturer) => {
-  const sql = `INSERT INTO lecturers (lecturer) VALUES ($1)`;
+  const sql = `INSERT INTO ${config.DATABASE_LECTURERS} (lecturer) VALUES ($1)`;
   const response = await query(sql, [lecturer]);
   return response.rows;
 };
 
 const updateLecturer = async (lecturer) => {
-  const sql = `UPDATE lecturers SET lecturer = $1 WHERE key = $2`;
+  const sql = `UPDATE ${config.DATABASE_LECTURERS} SET lecturer = $1 WHERE key = $2`;
   const response = await query(sql, [
     lecturer.updateValue,
     lecturer.updateRecord,
@@ -260,20 +249,20 @@ const updateLecturer = async (lecturer) => {
 };
 
 const getNextLecturerVal = async () => {
-  const sql = `SELECT last_value FROM lecturers_key_seq`;
+  const sql = `SELECT last_value FROM ${config.DATABASE_LECTURERKEYSEQ}`;
   const response = await query(sql);
   return response.rows;
 };
 
 /*** filter functions ***/
 const getUniqueTags = async () => {
-  const sql = `SELECT DISTINCT UNNEST(tags) AS value FROM videos ORDER BY value ASC`;
+  const sql = `SELECT DISTINCT UNNEST(tags) AS value FROM ${config.DATABASE_VIDEOS} ORDER BY value ASC`;
   const response = await query(sql);
   return response.rows;
 };
 
 const getUniqueWeek = async () => {
-  const sql = `SELECT DISTINCT(bootcamp_week) AS value FROM videos ORDER BY bootcamp_week ASC`;
+  const sql = `SELECT DISTINCT(bootcamp_week) AS value FROM ${config.DATABASE_VIDEOS} ORDER BY bootcamp_week ASC`;
   const response = await query(sql);
   return response.rows;
 };
@@ -281,7 +270,7 @@ const getUniqueWeek = async () => {
 const getUniqueSocLecturer = async () => {
   const sql = `SELECT DISTINCT(lecturer) 
                AS value
-               FROM videos 
+               FROM ${config.DATABASE_VIDEOS} 
                WHERE lecturer 
                IN (
                  SELECT DISTINCT(lecturer) 
@@ -294,7 +283,7 @@ const getUniqueSocLecturer = async () => {
 const getUniqueNonSocLecturer = async () => {
   const sql = `SELECT DISTINCT(lecturer)
                AS value
-               FROM videos 
+               FROM ${config.DATABASE_VIDEOS} 
                WHERE lecturer 
                NOT IN (
                  SELECT DISTINCT(lecturer) 
